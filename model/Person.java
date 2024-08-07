acdc840bacbe04c33cb3ff13d5d72abbde4d3fb9
Laura Monsalve <lmonsalve2219@gmail.com>
2024-08-07 13:50:31 -0500

    ADD new information about Program Class

---
+++ model/Person.java
@@ -0,0 +1,45 @@
+package app.model;
+
+public class Person {
+    //Attributes
+    private long id;
+    private long cedula;
+    private String name;
+    private long celphone;
+   
+    public Person(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(long cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getCelphone() {
+        return celphone;
+    }
+
+    public void setCelphone(long celphone) {
+        this.celphone = celphone;
+    }
+    
+    
+}
